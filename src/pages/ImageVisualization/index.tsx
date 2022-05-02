2a91f7d654cb4982da343963498c044fd4e497f2
Rômullo Cordeiro <romullocordeiro@gmail.com>
2022-05-01 23:37:17 -0300

    feat: implement wallpaper manager lib

--- src/pages/ImageVisualization/index.tsx
+++ src/pages/ImageVisualization/index.tsx
@@ -1,6 +1,9 @@
 import {RouteProp, useNavigation, useRoute} from '@react-navigation/core';
 import {StackNavigationProp} from '@react-navigation/stack';
 import React, {useState} from 'react';
+import {PermissionsAndroid} from 'react-native';
+import RNFetchBlob from 'rn-fetch-blob';
+import RnManageWallpaper from 'rn-manage-wallpaper';
 import MyButton from '../../components/Button';
 import {StackParamList} from '../../routes/app.routes';
 import {
@@ -25,7 +28,49 @@ const ImageVisualization = () => {
     navigation.goBack();
   };
 
-  const handleSetAs = () => {};
+  const handleSetAs = () => {
+    const writeGranted = PermissionsAndroid.request(
+      'android.permission.WRITE_EXTERNAL_STORAGE',
+      {
+        title: 'Wallpaper Paradise Storage Permission',
+        message:
+          'Wallpaper Paradise needs access to your storage ' +
+          'to save the wallpapers images',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK',
+      },
+    );
+    const readGranted = PermissionsAndroid.request(
+      'android.permission.READ_EXTERNAL_STORAGE',
+      {
+        title: 'Wallpaper Paradise Storage Permission',
+        message:
+          'Wallpaper Paradise needs access to your storage ' +
+          'to read the wallpapers images',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK',
+      },
+    );
+    if (!writeGranted || !readGranted) {
+      console.log('permissão negada');
+      return;
+    }
+    RNFetchBlob.config({
+      // add this option that makes response data to be stored as a file,
+      // this is much more performant.
+      fileCache: true,
+    })
+      .fetch('GET', image.url, {
+        //some headers ..
+      })
+      .then(res => {
+        try {
+          RnManageWallpaper.setWallpaper(`${res.path()}`, 2);
+        } catch (error) {
+          console.log(error);
+        }
+      });
+  };
 
   return (
     <Container>
