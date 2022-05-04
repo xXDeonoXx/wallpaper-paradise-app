c08c69d56c063a9e1de83a035a4c40df9a93cd78
Rômullo Cordeiro <romullocordeiro@gmail.com>
2022-05-04 09:20:45 -0300

    feat: dialog and toast implemented

--- src/pages/ImageVisualization/index.tsx
+++ src/pages/ImageVisualization/index.tsx
@@ -1,7 +1,7 @@
 import {RouteProp, useNavigation, useRoute} from '@react-navigation/core';
 import {StackNavigationProp} from '@react-navigation/stack';
 import React, {useState} from 'react';
-import {PermissionsAndroid} from 'react-native';
+import {Alert, PermissionsAndroid, ToastAndroid} from 'react-native';
 import RNFetchBlob from 'rn-fetch-blob';
 import RnManageWallpaper from 'rn-manage-wallpaper';
 import MyButton from '../../components/Button';
@@ -23,12 +23,13 @@ const ImageVisualization = () => {
   const navigation = useNavigation();
   const route = useRoute<RouteProp<StackParamList, 'ImageVisualization'>>();
   const [image] = useState(route?.params?.image);
+  const constants = RnManageWallpaper.getConstants();
 
   const handleBack = () => {
     navigation.goBack();
   };
 
-  const handleSetAs = () => {
+  const handleSetAs = async () => {
     const writeGranted = PermissionsAndroid.request(
       'android.permission.WRITE_EXTERNAL_STORAGE',
       {
@@ -55,7 +56,9 @@ const ImageVisualization = () => {
       console.log('permissão negada');
       return;
     }
-    RNFetchBlob.config({
+    let imagePath: string = '';
+
+    await RNFetchBlob.config({
       // add this option that makes response data to be stored as a file,
       // this is much more performant.
       fileCache: true,
@@ -65,11 +68,59 @@ const ImageVisualization = () => {
       })
       .then(res => {
         try {
-          RnManageWallpaper.setWallpaper(`${res.path()}`, 2);
+          imagePath = res.path();
         } catch (error) {
           console.log(error);
         }
       });
+
+    // error
+    if (!imagePath.length) return;
+    Alert.alert(
+      'Alert Title',
+      'My Alert Msg',
+      [
+        {
+          text: 'Home Screen',
+          onPress: () => setWallpaper(imagePath, constants.FLAG_SYSTEM),
+          style: 'default',
+        },
+        {
+          text: 'Lock Screen',
+          onPress: () => setWallpaper(imagePath, constants.FLAG_LOCK),
+          style: 'default',
+        },
+        {
+          text: 'Cancel',
+          // onPress: () => Alert.alert('Cancel Pressed'),
+          style: 'cancel',
+        },
+      ],
+      {
+        cancelable: true,
+      },
+    );
+  };
+
+  const setWallpaper = (path: string, flag: number) => {
+    try {
+      RnManageWallpaper.setWallpaper(`${path}`, flag);
+      ToastAndroid.showWithGravityAndOffset(
+        'Wallpaper successfully set',
+        ToastAndroid.SHORT,
+        ToastAndroid.BOTTOM,
+        25,
+        50,
+      );
+    } catch (error) {
+      ToastAndroid.showWithGravityAndOffset(
+        'Failed to set wallpaper',
+        ToastAndroid.SHORT,
+        ToastAndroid.BOTTOM,
+        25,
+        50,
+      );
+    }
   };
 
   return (
